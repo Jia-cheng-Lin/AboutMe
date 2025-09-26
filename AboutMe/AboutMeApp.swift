6e3638ebaec005299b5960fe450aa35401a3209f
Bill <bill092804@gmail.com>
2025-09-26 12:07:01 +0800

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by 陳芸萱 on 2025/9/26.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
